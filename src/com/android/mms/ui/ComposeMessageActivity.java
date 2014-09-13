a8503b618eb0a3f925ceb2857cf81482bdbbbd24
Rammar515 <rammarad515@gmail.com>
2014-09-13 22:03:12 +0200

    Build fix

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2946,16 +2946,6 @@ public class ComposeMessageActivity extends Activity
 
         buildAddAddressToContactMenuItem(menu);
 
-        // Add to Blacklist item (if enabled) and we are running on CyanogenMod
-        // This allows the app to be run on non-blacklist enabled roms (including Stock)
-        if (MessageUtils.isCyanogenMod(this)) {
-            if (BlacklistUtils.isBlacklistEnabled(this)) {
-                menu.add(0, MENU_ADD_TO_BLACKLIST, 0, R.string.add_to_blacklist)
-                        .setIcon(R.drawable.ic_block_message_holo_dark)
-                        .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-            }
-        }
-
         if (mConversation.getThreadId() > 0) {
             menu.add(0, MENU_CONVERSATION_OPTIONS, 0, R.string.menu_conversation_options);
         }
